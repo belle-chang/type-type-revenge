8701a0397c856b2fb3ae8c8423654fecfae4fdb8
Anabelle <bellech88@gmail.com>
2020-04-30 16:00:57 -0700

    experimented with dropping flower, added letter 'a' mesh

--- src/components/objects/Flower/Flower.js
+++ src/components/objects/Flower/Flower.js
@@ -1,7 +1,8 @@
 import { Group } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
-import MODEL from './flower.gltf';
+import MODEL from './a.gltf';
+import {Vector3} from 'three';
 
 class Flower extends Group {
     constructor(parent) {
@@ -14,15 +15,21 @@ class Flower extends Group {
             bob: true,
             spin: this.spin.bind(this),
             twirl: 0,
+            fall: this.fall.bind(this)
         };
 
         // Load object
         const loader = new GLTFLoader();
-
         this.name = 'flower';
         loader.load(MODEL, (gltf) => {
+            // console.log(gltf.scene.position);
+            // gltf.scene.position.x = -1;
+            // gltf.scene.rotation.x = 1.5708
+            // gltf.scene.rotation.y = 3.14159;
+            // console.log(gltf.scene.rotation.z)
             this.add(gltf.scene);
         });
+        // console.log(loader)
 
         // Add self to parent's update list
         parent.addToUpdateList(this);
@@ -30,6 +37,7 @@ class Flower extends Group {
         // Populate GUI
         this.state.gui.add(this.state, 'bob');
         this.state.gui.add(this.state, 'spin');
+        this.state.gui.add(this.state, 'fall');
     }
 
     spin() {
@@ -51,6 +59,18 @@ class Flower extends Group {
 
         // Start animation
         jumpUp.start();
+    }
+
+    fall() {
+        // this.state.twirl += 6 * Math.PI;
+
+        // Use timing library for more precice "bounce" animation
+        // TweenJS guide: http://learningthreejs.com/blog/2011/08/17/tweenjs-for-smooth-animation/
+        // Possible easings: http://sole.github.io/tween.js/examples/03_graphs.html
+        const fallDown = new TWEEN.Tween(this.position)
+            .to({ y: -100 }, 300);
+
+        fallDown.start();
     }
 
     update(timeStamp) {
