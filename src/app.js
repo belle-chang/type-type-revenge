e9c2b675b9ca57272120f08443b44daac677a37c
Christy Lee <christyl@princeton.edu>
2020-05-04 12:17:33 -0400

    add functionality to read from local json file

--- src/app.js
+++ src/app.js
@@ -6,20 +6,25 @@
  * handles window resizes.
  *
  */
-import * as THREE from 'three';
-import { WebGLRenderer, PerspectiveCamera, Vector3 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { SeedScene } from 'scenes';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
-import mp3 from './sounds/WiiThemeSong.mp3';
-import $ from 'jquery';
+import * as THREE from "three";
+import { WebGLRenderer, PerspectiveCamera, Vector3 } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { SeedScene } from "scenes";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import mp3 from "./sounds/WiiThemeSong.mp3";
+// import $ from 'jquery';
 
 // Initialize core ThreeJS components
 
 // const camera = new PerspectiveCamera();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new WebGLRenderer({ antialias: true });
 
 // Set up camera
@@ -30,10 +35,12 @@ camera.lookAt(new Vector3(0, 0, 0));
 let fovRad = camera.fov * (Math.PI / 180);
 
 // half of height:
-var height = camera.position.z * Math.sin(fovRad / 2) / Math.sin((Math.PI / 2) - (fovRad / 2));
+var height =
+  (camera.position.z * Math.sin(fovRad / 2)) /
+  Math.sin(Math.PI / 2 - fovRad / 2);
 
-// half of width: 
-var width = height / window.innerHeight * window.innerWidth;
+// half of width:
+var width = (height / window.innerHeight) * window.innerWidth;
 
 // initialize scene
 const scene = new SeedScene(width, height);
@@ -44,9 +51,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // renderer.setClearColor(0x000000,0.0);
 renderer.setPixelRatio(window.devicePixelRatio);
 const canvas = renderer.domElement;
-canvas.style.display = 'block'; // Removes padding below canvas
+canvas.style.display = "block"; // Removes padding below canvas
 document.body.style.margin = 0; // Removes margin around page
-document.body.style.overflow = 'hidden'; // Fix scrolling
+document.body.style.overflow = "hidden"; // Fix scrolling
 document.body.appendChild(canvas);
 
 // Set up controls
@@ -57,74 +64,73 @@ document.body.appendChild(canvas);
 // controls.maxDistance = 16;
 // controls.update();
 
-
 // ADD POSTPROCESSING EFFECT SUPPORT
-var composer = new EffectComposer( renderer );
+var composer = new EffectComposer(renderer);
 // first and mandatory pass
 composer.addPass(new RenderPass(scene, camera));
 
 // add glow
 // resolution, strength, kernel size, sigma?
 var bloomPass = new UnrealBloomPass(
-    new THREE.Vector2(window.innerWidth, window.innerHeight), 
-    3, 0.5, 0.1
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  3,
+  0.5,
+  0.1
 );
 bloomPass.renderToScreen = true;
 composer.addPass(bloomPass);
 
-
 // Render loop
-const onAnimationFrameHandler = (timeStamp) => {
-    // controls.update();
-    // renderer.render(scene, camera);
-    composer.render(timeStamp);
-    scene.update && scene.update(timeStamp);
-    window.requestAnimationFrame(onAnimationFrameHandler);
+const onAnimationFrameHandler = timeStamp => {
+  // controls.update();
+  // renderer.render(scene, camera);
+  composer.render(timeStamp);
+  scene.update && scene.update(timeStamp);
+  window.requestAnimationFrame(onAnimationFrameHandler);
 };
 window.requestAnimationFrame(onAnimationFrameHandler);
 
 // Resize Handler
 const windowResizeHandler = () => {
-    const { innerHeight, innerWidth } = window;
-    renderer.setSize(innerWidth, innerHeight);
-    // composer.setSize(innerWidth, innerHeight);
-    camera.aspect = innerWidth / innerHeight;
-    camera.updateProjectionMatrix();
+  const { innerHeight, innerWidth } = window;
+  renderer.setSize(innerWidth, innerHeight);
+  // composer.setSize(innerWidth, innerHeight);
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
 };
 windowResizeHandler();
-window.addEventListener('resize', windowResizeHandler, false);
+window.addEventListener("resize", windowResizeHandler, false);
 window.addEventListener("keydown", handleKeyDown);
 window.addEventListener("keyup", handleKeyUp);
 
 // when key is pressed save event key to key parameter of SeedScene
 function handleKeyDown(event) {
-    if (!event.metaKey && !event.altKey && !event.controlKey)
-        scene.key = event.key;
+  if (!event.metaKey && !event.altKey && !event.controlKey)
+    scene.key = event.key;
 }
 
 // once key is lifted, set SeedScene key to default value
 function handleKeyUp(event) {
-    scene.key = "";
+  scene.key = "";
 }
 
 // code below is taken from the Three.js website
 // create an AudioListener and add it to the camera
 var listener = new THREE.AudioListener();
-camera.add( listener );
+camera.add(listener);
 
 // create a global audio source
-var sound = new THREE.Audio( listener );
+var sound = new THREE.Audio(listener);
 
 // load a sound and set it as the Audio object's buffer
 var audioLoader = new THREE.AudioLoader();
-audioLoader.load( mp3, function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop( true );
-	sound.setVolume( 0.5 );
-	sound.play();
+audioLoader.load(mp3, function (buffer) {
+  sound.setBuffer(buffer);
+  sound.setLoop(true);
+  sound.setVolume(0.5);
+  sound.play();
 });
 
 // $.getJSON( "./json/exampleSong.json", function( json ) {
 //     console.log( "JSON Data: " + json.notes[ 3 ].note );
 //    });
-
